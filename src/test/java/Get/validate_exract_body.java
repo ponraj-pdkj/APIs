6e3858b283c80b477a8cca003114f36461670874
ponraj-hub <ponraj8785@gmail.com>
2024-08-25 07:55:49 +0530

    ok

---
+++ src/test/java/Get/validate_exract_body.java
@@ -0,0 +1,24 @@
+package com.get.rest;
+
+import static io.restassured.RestAssured.given;
+
+import org.testng.annotations.Test;
+
+import io.restassured.response.Response;
+
+public class validate_exract_body {
+ 
+	
+	public static void main(String[] args) {
+
+		extract_body();
+
+	}
+	
+	
+public static  void extract_body() {
+	
+	
+
+	}
+}
